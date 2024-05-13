8d0d08904763e0db74c74df728d6261a7f7ab896
Ijjoe <cijjj9220@gmail.com>
2024-05-13 11:19:42 +0900

    Update streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -4,23 +4,10 @@ Here's our first attempt at using data to create a table:
 """
 
 import streamlit as st
-import pandas as pd
-from io import StringIO
+import numpy as np
+from PIL import Image
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
-
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
-
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
-
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+img_file_buffer = st.file_uploader('Upload a PNG image', type='png')
+if img_file_buffer is not None:
+    image = Image.open(img_file_buffer)
+    img_array = np.array(image)
