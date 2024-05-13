a2278de5ac0cfb46ab704c95b906f322449b7d86
Ijjoe <cijjj9220@gmail.com>
2024-05-13 13:10:23 +0900

    n

--- streamlit_main.py
+++ streamlit_main.py
@@ -6,6 +6,7 @@ Here's our first attempt at using data to create a table:
 import streamlit as st
 import numpy as np
 from PIL import Image
+import matplotlib.pyplot as plt
 
 img_file_buffer = st.file_uploader('Upload a PNG image', type='png')
 if img_file_buffer is not None:
